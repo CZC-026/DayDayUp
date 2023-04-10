454bf6275025cba59853363888a4fdfe7a53ddf2
czc <m18801171456@163.com>
2023-04-10 16:54:20 +0800

    项目初始化“

---
+++ 2023_04_10/src/main/java/MyHashMap1.java
@@ -0,0 +1,16 @@
+/**
+ * @author Pers
+ * @requirements:1.实现一个支持泛型的hashmap；2.支持并发操作；3.允许插入重复的键值
+ */
+public class MyHashMap1 {
+    //链表+数组实现
+
+
+}
+
+class Node{
+    int key;
+    int value;
+    public
+}
+
